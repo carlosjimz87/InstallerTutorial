39b09e53860499fab4721c6b3b1d960a223075fc
carlosjimz87 <carlosjimz87@gmail.com>
2022-03-30 19:07:06 +0200

    minor refactors

--- app/src/main/java/com/carlosjimz87/installertutorial/DownloadController.kt
+++ app/src/main/java/com/carlosjimz87/installertutorial/DownloadController.kt
@@ -10,7 +10,6 @@ import android.os.Build
 import android.os.Environment
 import android.widget.Toast
 import androidx.core.content.FileProvider
-import com.carlosjimz87.installertutorial.Constants.APP_INSTALL_PATH
 import com.carlosjimz87.installertutorial.Constants.FILE_BASE_PATH
 import com.carlosjimz87.installertutorial.Constants.FILE_NAME
 import com.carlosjimz87.installertutorial.Constants.MIME_TYPE
@@ -18,20 +17,27 @@ import com.carlosjimz87.installertutorial.Constants.PROVIDER_PATH
 import timber.log.Timber
 import java.io.File
 
+enum class InstallMethod {
+    PROVIDER,
+    INTENT
+}
+
 class DownloadController(private val context: Context, private val url: String) {
 
 
     private var downloadManager: DownloadManager =
         context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
 
-    private var downloadID = -1L
+    private var prevId = -1L
+    private var prevUri: Uri? = null
+
     fun enqueueDownload() {
 
         var destination =
             context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString() + "/"
         destination += FILE_NAME
 
-        val uri = Uri.parse("$FILE_BASE_PATH$destination")
+        prevUri = Uri.parse("$FILE_BASE_PATH$destination")
 
         val file = File(destination)
         if (file.exists()) file.delete()
@@ -43,11 +49,11 @@ class DownloadController(private val context: Context, private val url: String) 
         request.setDescription(context.resources.getString(R.string.downloading))
 
         // set destination
-        request.setDestinationUri(uri)
-        Timber.d("Downloading ${file.name} from $uri to $destination")
-        showInstallOption(destination, uri)
+        request.setDestinationUri(prevUri)
+        Timber.d("Downloading ${file.name} from $prevUri to $destination")
+        showInstallOption(destination)
         // Enqueue a new download and same the referenceId
-        downloadID = downloadManager.enqueue(request)
+        prevId = downloadManager.enqueue(request)
         Toast.makeText(context, context.getString(R.string.downloading), Toast.LENGTH_LONG)
             .show()
 
@@ -55,8 +61,7 @@ class DownloadController(private val context: Context, private val url: String) 
     }
 
     private fun showInstallOption(
-        destination: String,
-        uri: Uri
+        destination: String
     ) {
 
         // set BroadcastReceiver to install app when .apk is downloaded
@@ -65,48 +70,94 @@ class DownloadController(private val context: Context, private val url: String) 
                 context: Context,
                 intent: Intent
             ) {
-                if (downloadID == -1L) return
+                Timber.d("Download completed ${intent.extras.toString()}")
+                val newDownloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    val contentUri = FileProvider.getUriForFile(
+                if (newDownloadId == -1L) return
+
+                val newUri: Uri?
+                val method = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    newUri = getInstallUri(InstallMethod.PROVIDER, destination = destination)
+                    InstallMethod.PROVIDER
+                } else {
+                    newUri = getInstallUri(InstallMethod.INTENT, newDownloadId = prevId)
+                    InstallMethod.INTENT
+                }
+
+                if (checkDownloadedFile(prevUri)) {
+                    install(newUri, method)
+                }
+
+                Timber.d("Unregister receiver")
+                context.unregisterReceiver(this)
+            }
+
+
+        }
+        context.registerReceiver(
+            onComplete,
+            IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+        )
+    }
+
+
+    fun checkDownloadedFile(uri: Uri?): Boolean {
+        Timber.d("Verify file at ${uri?.path}")
+        return uri?.path?.let { path ->
+            File(path).run {
+                this.exists() && this.isFile && this.canRead()
+            }
+        } ?: false
+    }
+
+    fun getInstallUri(
+        method: InstallMethod,
+        newDownloadId: Long? = null,
+        destination: String? = null
+    ): Uri? {
+        return when (method) {
+            InstallMethod.PROVIDER -> {
+                destination?.let {
+                    FileProvider.getUriForFile(
                         context,
                         BuildConfig.APPLICATION_ID + PROVIDER_PATH,
                         File(destination)
                     )
-                    val install = Intent(Intent.ACTION_VIEW)
-                    install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                    install.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                    install.putExtra(Intent.EXTRA_NOT_UNKNOWN_SOURCE, true)
-                    install.data = contentUri
-                    context.startActivity(install)
-                    context.unregisterReceiver(this)
-                    // finish()
-                } else {
-                    Timber.d("Lower apis")
-                    val install = Intent(Intent.ACTION_VIEW)
-                    install.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-
-                    val newDownloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-                    val newUri: Uri? = downloadManager.getUriForDownloadedFile(newDownloadId)
-
-                    if (newDownloadId == downloadID && uri.path?.equals(newUri) == true) {
-                        install.setDataAndType(
-                            uri,
-                            downloadManager.getMimeTypeForDownloadedFile(downloadID)
-                        );
-
-                        install.setDataAndType(
-                            uri,
-                            APP_INSTALL_PATH
-                        )
-                        context.startActivity(install)
-                    }
-
-                    context.unregisterReceiver(this)
-                    // finish()
+                }
+            }
+            InstallMethod.INTENT -> {
+                newDownloadId?.let {
+                    downloadManager.getUriForDownloadedFile(newDownloadId)
                 }
             }
         }
-        context.registerReceiver(onComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
     }
+
+    fun install(uri: Uri?, method: InstallMethod) {
+        Timber.d("Proceed to install on $${Build.VERSION.CODENAME} via $method")
+        val intent: Intent = when (method) {
+            InstallMethod.PROVIDER -> {
+                Intent(Intent.ACTION_VIEW).apply {
+                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    putExtra(Intent.EXTRA_NOT_UNKNOWN_SOURCE, true)
+                    data = uri
+                }
+            }
+            InstallMethod.INTENT -> {
+                val mime = downloadManager.getMimeTypeForDownloadedFile(prevId)
+                Intent(Intent.ACTION_VIEW).apply {
+                    flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    setDataAndType(
+                        uri,
+                        mime
+                    )
+                }
+            }
+        }
+        Timber.d("Installing $intent")
+        context.startActivity(intent)
+    }
+
 }
+
